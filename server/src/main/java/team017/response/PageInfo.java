7fd0f3aac8b992fe259513fe46929749403d02dd
godjih <sks6073@gmail.com>
2022-11-10 14:15:59 +0900

    feat : 글로벌 클래스 구현

--- server/src/main/java/team017/response/PageInfo.java
+++ server/src/main/java/team017/response/PageInfo.java
@@ -1,8 +1,10 @@
 package team017.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class PageInfo {
     private int page;
     private int size;
