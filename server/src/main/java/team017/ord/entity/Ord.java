ea2f8b638fe372572003517aa49a7c355caec0f0
godjih <sks6073@gmail.com>
2022-11-11 17:58:09 +0900

    feat : Ord초기 기능 구현

---
+++ server/src/main/java/team017/ord/entity/Ord.java
@@ -0,0 +1,33 @@
+package team017.ord.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Ord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ordId;
+
+    @Column(length = 50, nullable = false)
+    private String address;
+
+    @Column(length = 13, nullable = false, unique = true)
+    private String phone;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column()
+    private String status;
+
+    //상품 PK OneToMany
+//  @OneToMany(mappedBy = "ord", targetEntity = product.class)
+
+
+    //소비자 PK OneToMany
+//   @OneToMany(mappedBy = "ord", targetEntity = client.class)
+
+}
