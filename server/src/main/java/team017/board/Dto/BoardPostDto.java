a454c78049bcfe4bc6d88a579ef3962bb48ce5bb
Shin-seung-hyun <jjack1028@naver.com>
2022-11-11 20:01:03 +0900

    #15 feat: Board 초기 기능 구현

--- server/src/main/java/team017/board/Dto/BoardPostDto.java
+++ server/src/main/java/team017/board/Dto/BoardPostDto.java
@@ -1,4 +1,30 @@
 package team017.board.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import team017.member.entity.Seller;
+import team017.product.Entity.Product;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardPostDto {
+
+    private Long sellerId;
+
+    private String title;
+
+    private String content;
+
+    private int price;
+
+    private int stock;
+
+    private String category;
+
 }
