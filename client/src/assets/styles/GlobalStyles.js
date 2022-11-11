d8ac84699d206c1b3555f18a4f205fd1e95ea51c
waymokorea <taw0207@naver.com>
2022-11-11 20:57:25 +0900

    #7 chore: Global Styles 위치 변경

--- client/src/assets/styles/GlobalStyles.js
+++ client/src/assets/styles/GlobalStyles.js
@@ -8,7 +8,6 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family:  -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;;
     font-size: 100%;
   }
 
