ffdd53c2a78ac8b7a03b1d7b75a75d6bf916d10e
Shin-seung-hyun <jjack1028@naver.com>
2022-11-12 21:24:00 +0900

    #19 feat: 상품 create 기능 구현

--- server/src/main/java/team017/product/Entity/Product.java
+++ server/src/main/java/team017/product/Entity/Product.java
@@ -1,35 +1,45 @@
-// package team017.product.Entity;
-//
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-// import lombok.Setter;
-// import org.hibernate.annotations.ColumnDefault;
-// import org.hibernate.annotations.DynamicInsert;
-//
-// import javax.persistence.*;
-//
-// @Getter
-// @Setter
-// @Entity
-// @NoArgsConstructor
-// @Table(name = "product")
-// public class Product {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long productId;
-//
-//     @Column(nullable = false)
-//     private int price;
-//
-//     @ColumnDefault("selling")
-//     //@Column(columnDefinition = "")
-//     @Column(nullable = true)
-//     private String status; //판매 상태
-//
-//     @Column(nullable = false)
-//     private int stock;
-//
-//     @Column(nullable = false)
-//     private int category; //상품분류 (과일 :1 , 채소: 2, 곡물: 3,  견과류: 4)
-//
-// }
+package team017.product.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import team017.board.Entity.Board;
+import team017.member.entity.Seller;
+
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@DynamicInsert
+@Table(name = "Product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @Column(nullable = false)
+    private int price;
+
+    @ColumnDefault(value ="'selling'")
+    private String status; //판매 상태
+
+    @Column(nullable = false)
+    private int stock;
+
+    @Column(nullable = false)
+    private int category; //상품분류 (과일 :1 , 채소: 2, 곡물: 3,  견과류: 4)
+
+
+    @OneToOne(mappedBy = "product",cascade = {CascadeType.REMOVE}, targetEntity = Board.class )
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "sellerId" , referencedColumnName = "sellerId")
+    private Seller seller;
+
+}
--- server/src/main/java/team017/product/Repository/ProductRepository.java
+++ server/src/main/java/team017/product/Repository/ProductRepository.java
@@ -1,4 +1,11 @@
 package team017.product.Repository;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import team017.product.Entity.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
 }
