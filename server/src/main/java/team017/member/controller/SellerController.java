8997fa59fef5f670a467aa86b2677bdb51c2e62c
Bhinney <107696895+Bhinney@users.noreply.github.com>
2022-11-12 23:24:56 +0900

    feat: mapping entities (#42)

--- server/src/main/java/team017/member/controller/SellerController.java
+++ server/src/main/java/team017/member/controller/SellerController.java
@@ -4,12 +4,15 @@ import javax.validation.constraints.Positive;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import team017.member.dto.MemberDto;
+import team017.member.dto.SellerPatchDto;
 import team017.member.entity.Member;
 import team017.member.entity.Seller;
 import team017.member.mapper.MemberMapper;
@@ -27,11 +30,18 @@ public class SellerController {
 
 	/* 판매자 마이 페이지 조회 */
 	@GetMapping("/{seller_id}")
-	public ResponseEntity findSeller(@PathVariable("seller_id") @Positive long sellerId) {
+	public ResponseEntity getSeller(@PathVariable("seller_id") @Positive long sellerId) {
 		Seller seller = sellerService.findVerifiedSeller(sellerId);
 		Member member = memberService.findVerifiedMember(seller.getMember().getMemberId());
 		MemberDto.SellerDto response = mapper.memberToSellerDto(member, seller);
 
 		return ResponseEntity.ok(response);
+	}
+
+	/* 판매자 정보 수정 */
+	@PatchMapping("/{seller_id}")
+	public ResponseEntity patchSeller(@PathVariable("seller-id") @Positive long sellerId,
+			@RequestBody SellerPatchDto sellerPatchDto) {
+		return null;
 	}
 }
--- server/src/main/java/team017/member/service/SellerService.java
+++ server/src/main/java/team017/member/service/SellerService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import team017.member.entity.Member;
 import team017.member.entity.Seller;
 import team017.member.repository.SellerRepository;
 
@@ -12,11 +13,20 @@ import team017.member.repository.SellerRepository;
 @RequiredArgsConstructor
 public class SellerService {
 	private final SellerRepository sellerRepository;
+	private final MemberService memberService;
 
 	/* 존재하는 생산자인지 확인 + 생산자 정보 리턴 */
 	public Seller findVerifiedSeller(long sellerId) {
 		Optional<Seller> optionalSeller = sellerRepository.findById(sellerId);
 		Seller findSeller = optionalSeller.orElseThrow(() -> new RuntimeException("Seller Not Found"));
 		return findSeller;
+	}
+
+	/* 생산자 정보 수정 */
+	public Seller updateSeller(Seller seller) {
+		Seller findSeller = findVerifiedSeller(seller.getSellerId());
+		Member findMember = memberService.findVerifiedMember(seller.getMember().getMemberId());
+
+		return null;
 	}
 }
