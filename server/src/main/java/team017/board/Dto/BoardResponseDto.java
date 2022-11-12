45a6082ee900c556d2a304064c115afacdc366a5
Shin-seung-hyun <jjack1028@naver.com>
2022-11-12 21:23:16 +0900

    #15 feat: 판매 게시글 create 기능 구현

--- server/src/main/java/team017/board/Dto/BoardResponseDto.java
+++ server/src/main/java/team017/board/Dto/BoardResponseDto.java
@@ -1,4 +1,36 @@
 package team017.board.Dto;
+import lombok.*;
 
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardResponseDto {
+
+    private Long boardId;
+
+    private Long productId;
+
+    private Long sellerId;
+
+    private String title;
+
+    private String content;
+
+    private int price;
+
+    private int stock;
+
+    private int category; //상품분류 (과일 :1 , 채소: 2, 곡물: 3,  견과류: 4)
+
+    private String status; //판매 상태
+
+    private int viewCount;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime modifiedAt;
+
 }
--- server/src/main/java/team017/board/Repository/BoardRepository.java
+++ server/src/main/java/team017/board/Repository/BoardRepository.java
@@ -1,4 +1,9 @@
 package team017.board.Repository;
 
-public interface BoardRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import team017.board.Entity.Board;
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
 }
