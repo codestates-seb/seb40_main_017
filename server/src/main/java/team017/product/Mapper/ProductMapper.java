c298e3ec6bead3bdde859cc00c04c7b23af15a5a
Shin-seung-hyun <jjack1028@naver.com>
2022-11-14 13:05:25 +0900

    #43 feat: 상품 create 기능 구현 완료

--- server/src/main/java/team017/product/Mapper/ProductMapper.java
+++ server/src/main/java/team017/product/Mapper/ProductMapper.java
@@ -1,11 +1,14 @@
 package team017.product.Mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import team017.board.Dto.BoardPostDto;
+import team017.member.entity.Seller;
 import team017.product.Entity.Product;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
     Product boardPostDtoToProduct(BoardPostDto boardPostDto);
+
 }
--- server/src/main/java/team017/product/Service/ProductService.java
+++ server/src/main/java/team017/product/Service/ProductService.java
@@ -1,4 +1,24 @@
 package team017.product.Service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import team017.global.Exception.BusinessLogicException;
+import team017.global.Exception.ExceptionCode;
+import team017.product.Entity.Product;
+import team017.product.Repository.ProductRepository;
+
+import java.util.Optional;
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Product findVerifiedProduct(long productId){
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+        Product findProduct = optionalProduct.orElseThrow(()->
+                new BusinessLogicException(ExceptionCode.PRODUCT_NOT_FOUND));
+
+        return findProduct;
+    }
 }
