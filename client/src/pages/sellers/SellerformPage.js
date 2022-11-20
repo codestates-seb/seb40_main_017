aa0c72ff7a7cda49c414a50d5396cca9f2c12fbd
waymokorea <taw0207@naver.com>
2022-11-20 22:20:50 +0900

    #57 feat: Multistepform 페이지 구현

---
+++ client/src/pages/sellers/SellerformPage.js
@@ -0,0 +1,137 @@
+import { MultiStepProgressBar } from '../../components/MultiStepProgressBar';
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import { MultiStepForm } from '../../components/MultiStepForm';
+import { LineWobble } from '@uiball/loaders';
+
+const FormLayout = styled.div`
+  background: var(--off-white);
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SellerPostHeader = styled.div`
+  width: 39.5em;
+  height: 50px;
+  margin-bottom: 30px;
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+  font-weight: 500;
+  gap: 10px;
+  border-bottom: 5px solid var(--green);
+  .head {
+    color: var(--green);
+  }
+`;
+
+const FormBox = styled.div`
+  position: relative;
+  font-size: 30px;
+  width: 40em;
+  height: 25em;
+  padding: 2em;
+  background: var(--white);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 0.5em;
+  box-shadow: 0px 4px 5px 1px #8e8e8e;
+`;
+
+const BackImage = styled.div`
+  position: absolute;
+  bottom: 0px;
+  left: 1em;
+  width: 200px;
+  height: 200px;
+  background: url('https://ifh.cc/g/y7qhrw.png');
+  z-index: 1;
+`;
+
+const ProgressBarBox = styled.div`
+  width: 20em;
+  height: 2em;
+  margin-bottom: 2em;
+`;
+
+const FormBodyBox = styled.div`
+  width: 30em;
+  height: 15em;
+  display: flex;
+  justify-content: center;
+  z-index: 10;
+`;
+
+// const ButtonBox = styled.div`
+//   width: 50%;
+//   display: flex;
+//   justify-content: space-between;
+// `;
+
+function SellerformPage() {
+  const [index, setIndex] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    title: '',
+    stock: '',
+    price: '',
+    category: '',
+    mainimage: '',
+    content: '',
+  });
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
+  const prevButton = () => {
+    if (index > 1) {
+      setIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+  const nextButton = () => {
+    if (index < 5) {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  return (
+    <FormLayout>
+      <SellerPostHeader>
+        <p className="head">판매 물품</p>
+        <p>등록</p>
+      </SellerPostHeader>
+      <FormBox>
+        <ProgressBarBox>
+          <MultiStepProgressBar step={index} />
+        </ProgressBarBox>
+        <FormBodyBox>
+          {!isLoading && (
+            <MultiStepForm
+              index={index}
+              nextButton={nextButton}
+              prevButton={prevButton}
+              formData={formData}
+              setFormData={setFormData}
+              setIsLoading={setIsLoading}
+            />
+          )}
+          {isLoading && <LineWobble className="animation" size={150} lineWeight={5} speed={1.75} color="var(--green)" />}
+        </FormBodyBox>
+        {/* <ButtonBox>
+          <button onClick={prevButton} disabled={index === 1}>
+            Previous
+          </button>
+          {index === 4 ? <button>Submit</button> : <button onClick={nextButton}>Next</button>}
+        </ButtonBox> */}
+        <BackImage />
+      </FormBox>
+    </FormLayout>
+  );
+}
+
+export default SellerformPage;
