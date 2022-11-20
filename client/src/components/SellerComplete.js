1a0050b1d69791ca9b534f4d940dcb6e06aa7525
waymokorea <taw0207@naver.com>
2022-11-20 22:19:59 +0900

    #57 feat: multistepform 마지막작성 완료 알림 컴포넌트

---
+++ client/src/components/SellerComplete.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import { FiCheckCircle } from 'react-icons/fi';
+
+const CompleteBox = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-bottom: 6em;
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+const ButtonBox = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  position: absolute;
+  bottom: -2em;
+  button {
+    color: var(--black);
+    width: 10em;
+    height: 3em;
+    font-size: 16px;
+    background: var(--white);
+    border-radius: 15px;
+    border: 3px solid var(--green);
+    transition: 0.25s;
+    :hover {
+      background: var(--green);
+      color: var(--white);
+      transform: scale(1.2);
+    }
+  }
+`;
+
+export const SellerComplete = () => {
+  const handleOnClick = () => {
+    console.log('등록완료');
+  };
+
+  return (
+    <>
+      <CompleteBox>
+        <FiCheckCircle size={80} color="var(--green)" />
+        <h3>판매물품</h3>
+        <h3>등록이 완료 되었습니다</h3>
+        <ButtonBox>
+          <button onClick={handleOnClick}> 확인</button>
+        </ButtonBox>
+      </CompleteBox>
+    </>
+  );
+};
