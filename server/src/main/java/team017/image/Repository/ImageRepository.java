34464b31e9b1591e2ff8ef4d64f6e46252d8ae1b
Shin-seung-hyun <jjack1028@naver.com>
2022-11-21 11:22:09 +0900

    fix: conflict 수정

--- server/src/main/java/team017/image/Repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package team017.image.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import team017.image.Entity.Image;
-
-@Repository
-public interface ImageRepository extends JpaRepository<Image, Long>{
-}
