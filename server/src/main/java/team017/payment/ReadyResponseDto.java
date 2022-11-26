1863ff8f19d6d10ae0efa9c2aa0abd3c2bd6eca0
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2022-11-26 18:20:22 +0900

    #66 feat: 주문,결제 실패 시 잔여 수량 변경 기능 구현 (#117)
    * #108 fix: 연관관계 매핑 수정
    
    * #66 feat: 주문,결제 실패 시 잔여 수량 변경 기능 구현

--- server/src/main/java/team017/payment/ReadyResponseDto.java
+++ server/src/main/java/team017/payment/ReadyResponseDto.java
@@ -10,6 +10,6 @@ public class ReadyResponseDto {
 
     private String tid;                     //결제 고유 번호, 20자
     private String next_redirect_pc_url;    //사용자 정보 입력 화면 Redirect URL
-    private String partner_order_id;        //가맹점 주문번호, 최대 100자
+    private String partner_order_id;        //주문 Id
 
 }
