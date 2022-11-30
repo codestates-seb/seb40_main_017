aad859071244fff6600cfbb93d1574d110e9aa05
Shin-seung-hyun <jjack1028@naver.com>
2022-11-30 11:02:59 +0900

    #11 refactor: Exception code 리펙토링

--- server/src/main/java/team017/global/Exception/ExceptionCode.java
+++ server/src/main/java/team017/global/Exception/ExceptionCode.java
@@ -4,13 +4,15 @@ import lombok.Getter;
 
 public enum ExceptionCode {
 
-	PRODUCT_NOT_FOUND(404, "PRODUCT not found"),
+	/* 상품,판매 게시판 관련 예외 */
+	BOARD_NOT_FOUND(404, "판매 게시판을 찾을 수 없습니다."),
+	PRODUCT_SOLDOUT(404, "상품이 매진되었습니다."),
+	PRODUCT_NOT_FOUND(404, "상품을 찾을 수 없습니다."),
+	PRODUCT_NOT_ENOUGH(404, "재고가 충분하지 않습니다."),
+	COMMENT_NOT_FOUND(404, "문의를 찾을 수 없습니다."),
 
-	BOARD_NOT_FOUND(404, "BOARD not found"),
-	UNAUTHORIZED_MEMBER(403, "not authorized user"),
-	COMMENT_NOT_FOUND(404, "Comment not found"),
-	PRODUCT_SOLDOUT(404, "Product soldout"),
-	PRODUCT_NOT_ENOUGH(404, "Product not enough"),
+	/* 주문 관련 예외 */
+	ORDER_NOT_FOUND(404, "주문을 찾을 수 없습니다."),
 
 	/* 리뷰 관련 예외 */
 	REVIEW_NOT_FOUND(404, "리뷰가 존재하지 않습니다."),
@@ -21,6 +23,7 @@ public enum ExceptionCode {
 	ROLE_ERROR(400, "잘못된 역할입니다."),
 	PROVIDER_ERROR(400, "로컬 사용자인지 혹은 정확한 소셜 사용자인지 확인하세요."),
 	MEMBER_EXISTS(409, "이미 존재하는 이메일입니다."),
+	UNAUTHORIZED_MEMBER(403, "권한이 없는 사용자 입니다."),
 
 	/* Security Error Message */
 	NOT_FOUND_AUTHORITIES(400, "권한이 없는 토큰입니다."),
@@ -31,11 +34,7 @@ public enum ExceptionCode {
 	LOGOUT_MEMBER(400,"로그아웃 된 사용자입니다."),
 	UNAUTHORIZED(401, "유효하지 않은 인증입니다."),
 	FORBIDDEN(403, "접근 권한이 없습니다."),
-	NOT_FOUND_TOKEN(404, "토큰을 찾을 수 없습니다."),
-
-	/* 주문 관련 예외 */
-	ORDER_NOT_FOUND(404, "Order not found");
-
+	NOT_FOUND_TOKEN(404, "토큰을 찾을 수 없습니다.");
 
 	@Getter
 	private int status;
