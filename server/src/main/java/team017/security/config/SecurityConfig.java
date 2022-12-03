30dc0ad07c4b58a9cab43941da55d3e277fa2a50
Bhinney <snakellie97@gmail.com>
2022-12-03 14:13:24 +0900

    fix: 의심부분 주석

--- server/src/main/java/team017/security/config/SecurityConfig.java
+++ server/src/main/java/team017/security/config/SecurityConfig.java
@@ -30,8 +30,8 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.headers().frameOptions().sameOrigin()
-			.and()
+			// .headers().frameOptions().sameOrigin()
+			// .and()
 			.csrf().disable()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
