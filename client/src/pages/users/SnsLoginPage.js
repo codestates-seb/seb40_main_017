837a970fe0d81e22fe6a65b157b0da6fffc64a44
MinSeongKiim <alstjddl0541@gmail.com>
2022-12-05 23:01:00 +0900

    #214 feat: 소셜 로그인 이후 role 구분

--- client/src/pages/users/SnsLoginPage.js
+++ client/src/pages/users/SnsLoginPage.js
@@ -2,6 +2,11 @@ import { useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { snsLogin } from '../../api/login';
+import { snsSignupMember } from '../../api/signup';
+import { SubmitButton } from '../../components/Button';
+import Container from '../../components/Container';
+import { Form, FormRadioGroup } from '../../components/Form';
+import { useInput } from '../../components/Input';
 
 const SnsLoginPage = () => {
   const dispatch = useDispatch();
@@ -9,6 +14,7 @@ const SnsLoginPage = () => {
 
   const params = new URLSearchParams(useLocation().search);
   const accessToken = params.get('accessToken');
+  const memberId = params.get('memberId');
 
   const handleSuccessCallback = useCallback((status) => {
     if (status) {
@@ -17,10 +23,47 @@ const SnsLoginPage = () => {
   }, []);
 
   useEffect(() => {
-    dispatch(snsLogin({ accessToken }, handleSuccessCallback));
+    if (memberId === null) {
+      dispatch(snsLogin({ accessToken }, handleSuccessCallback));
+    }
   }, []);
 
-  return <>SNS 로그인 처리 중입니다.</>;
+  const [userRole, handleChangeUserRole] = useInput('');
+
+  const handleSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      if (userRole === '') {
+        alert('회원 구분을 선택해주세요.');
+      } else {
+        dispatch(snsSignupMember({ memberId, userRole }, handleSuccessCallback));
+      }
+    },
+    [dispatch, handleSuccessCallback, memberId, userRole]
+  );
+
+  return (
+    <>
+      {memberId === null ? (
+        <>SNS 로그인 중입니다...</>
+      ) : (
+        <Container name="소셜 회원가입">
+          <Form onSubmit={handleSubmit}>
+            <FormRadioGroup
+              name="role"
+              onChange={handleChangeUserRole}
+              items={[
+                { value: 'SELLER', text: '판매자', checked: false },
+                { value: 'CLIENT', text: '구매자', checked: false },
+              ]}
+            />
+            <SubmitButton>가입완료</SubmitButton>
+          </Form>
+        </Container>
+      )}
+    </>
+  );
 };
 
 export default SnsLoginPage;
