df65292c69dcb0ba1ef040d7a4a79d980aaee836
Ji Hoon <97619571+Godjih@users.noreply.github.com>
2022-12-05 11:23:09 +0900

    #200 Feat : 'github actions 기능 구현' (#202)
    * Feat : 'github actions 기능 구현'
    
    * Fix : 'test build 안되는 현상 고치기'

--- server/src/test/java/team017/util/ApiDocumentUtils.java
+++ server/src/test/java/team017/util/ApiDocumentUtils.java
@@ -1,16 +1,16 @@
-package team017.util;
-
-import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
-import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
-
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-
-public class ApiDocumentUtils {
-    public static OperationRequestPreprocessor getRequestPreProcessor() {
-        return preprocessRequest(prettyPrint());
-    }
-
-    public static OperationResponsePreprocessor getResponsePreProcessor() {
-        return preprocessResponse(prettyPrint());
-    }
-}
+//package team017.util;
+//
+//import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+//import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+//
+//import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+//
+//public class ApiDocumentUtils {
+//    public static OperationRequestPreprocessor getRequestPreProcessor() {
+//        return preprocessRequest(prettyPrint());
+//    }
+//
+//    public static OperationResponsePreprocessor getResponsePreProcessor() {
+//        return preprocessResponse(prettyPrint());
+//    }
+//}
