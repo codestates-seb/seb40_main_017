e8c1480cc510644a56a8036270be2f676dab0d51
godjih <sks6073@gmail.com>
2022-12-05 11:04:48 +0900

    Feat : 'github actions 구현완료'

--- server/src/test/java/team017/ServerApplicationTests.java
+++ server/src/test/java/team017/ServerApplicationTests.java
@@ -3,10 +3,10 @@ package team017;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ServerApplicationTests {
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 }
 
