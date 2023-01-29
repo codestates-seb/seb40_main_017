c99e9de235aaa0ff7b9f2198c0be3b5b98bc497f
godjih <sks6073@gmail.com>
2023-01-29 18:10:54 +0900

    FEAT :'github actions 기능 test'

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,6 +23,7 @@ else
 fi
 
 
+
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포 $DEPLOY_JAR"    >> /home/ubuntu/action/deploy.log
 sudo nohup java -jar $DEPLOY_JAR --spring.profiles.active=server >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
